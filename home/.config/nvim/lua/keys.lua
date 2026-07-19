5209eee05cb3efd88113b2667e22e4236104ab8a
eungizoa <eungizoa@gmail.com>
2026-07-19 22:56:12 +0900

    [fix] unable esc autosave option

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -1,5 +1,5 @@
 -- save by pressing Escape
-vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
+-- vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
 -- select all
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 -- pasting over a selection no longer clobbers your clipboard
