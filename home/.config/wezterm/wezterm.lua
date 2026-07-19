0ea381ec09d016cfa1827b968b77f96307584f06
eungizoa <eungizoa@gmail.com>
2026-07-20 02:25:33 +0900

    [fix] keep wezterm tab bar visible with a single tab

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "RESIZE"
 
 return config
